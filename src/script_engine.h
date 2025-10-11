b7638ab3f433dc02e022654db1d0f90a4a2a0bd9
Haruka Kataoka <hrk8@drumsoft.com>
2025-10-12 00:28:33 +0900

    Corrected errors in applying LICENSE. Removed redundant copyright-related comments.

--- src/script_engine.h
+++ src/script_engine.h
@@ -1,25 +1,6 @@
 #ifndef SCRIPT_ENGINE_H
 #define SCRIPT_ENGINE_H
-// Otojs - live sound programming environment with JavaScript.
-// otojsd - sound processing server for Otojs.
 // Otojsd::ScriptEngine - JavaScript engine wrapper for otojsd.
-/*
-    Otojs - live sound programming environment with JavaScript.
-    Copyright (C) 2025- Haruka Kataoka
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include <libplatform/libplatform.h>
 #include <v8.h>
